4ec68f628f882c883729b038a63b0c8c6f3a241a
Vincent van Bergen <v.vanbergen@gmail.com>
2015-10-19 16:29:01 +0200

    some stuff

--- news_retrieval/models.py
+++ news_retrieval/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsArticle(models.Model):
+    google_id = models.CharField('id', max_length=200)
+    link = models.CharField('link', max_length=400)
+    published = models.DateTimeField('published')
+    summary = models.TextField('summary')
+    title = models.CharField('title', max_length=200)
+
+    def __str__(self):
+        return self.title
