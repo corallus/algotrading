e8eea782753b43ece5be171ec5689841ea7296f4
Kees Hendriks <khendriks9@gmail.com>
2015-10-19 18:32:54 +0200

    twitter will now get up to 100 new messages, so no doubles (this is not saved in database) So will get doubles when restarted

--- social_retrieval/views.py
+++ social_retrieval/views.py
@@ -1,15 +1,13 @@
-import hashlib
-import random
-from time import time
-from urllib import request as url_request
-import urllib.parse
+import codecs
+import json
 from django.conf import settings
-from django.core.serializers import json
-from django.utils.baseconv import base64
 from django.views.generic import TemplateView
 import oauth2
+from social_retrieval.models import Tweet
+from time import strptime, strftime
 
 STOCKS = ['toyota', 'netflix', 'asml', 'volkswagen']
+SINCE_ID = {stock: -1 for stock in STOCKS}
 
 
 class FacebookView(TemplateView):
@@ -70,7 +68,33 @@ class TwitterView(TemplateView):
         access_token_secret = twitter_user.access_token['oauth_token_secret']
 
         for stock in STOCKS:
-            search = self.oauth_req('https://api.twitter.com/1.1/search/tweets.json?q=%s' % stock,
-                                    access_token, access_token_secret)
-            print(search)
+            baseurl = 'https://api.twitter.com/1.1/search/tweets.json?q=%s' % stock
+            paramdict = {'lang': 'en', 'count': 100}
+            if SINCE_ID[stock] != -1:
+                paramdict['since_id'] = SINCE_ID[stock]
+            params = ''
+            for key, value in paramdict.items():
+                params += '&%s=%s' % (key, value)
+            url = baseurl + params
+            print(url)
+            search = self.oauth_req(url, access_token, access_token_secret)
+            str_search = search.decode('utf-8')
+            dict = json.loads(str_search)
+            for tweet in dict['statuses']:
+                tweet_dict = {'created_at': strftime('%Y-%m-%d %H:%M:%S', strptime(tweet['created_at'], '%a %b %d %H:%M:%S +0000 %Y')),
+                              'favorite_count': tweet['favorite_count'],
+                              'tweet_id': tweet['id'], 'in_reply_to_status_id': tweet['in_reply_to_status_id'],
+                              'is_quote_status': tweet['is_quote_status'], 'retweet_count': tweet['retweet_count'],
+                              'text': tweet['text'], 'user_id': tweet['user']['id'],
+                              'user_followers_count': tweet['user']['followers_count'],
+                              'user_favourites_count': tweet['user']['favourites_count'],
+                              'user_friends_count': tweet['user']['friends_count'],
+                              'user_listed_count': tweet['user']['listed_count'], 'user_name': tweet['user']['name'],
+                              'user_screen_name': tweet['user']['screen_name'],
+                              'user_statuses_count': tweet['user']['statuses_count']
+                              }
+                Tweet.objects.create(**tweet_dict)
+                stock_list.append(tweet_dict)
+                SINCE_ID[stock] = max(SINCE_ID[stock], tweet['id'])
+
         return stock_list
