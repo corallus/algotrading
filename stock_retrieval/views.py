fbecb3bacbd7158f7f5258257f38f2d51993bf59
Vincent van Bergen <v.vanbergen@gmail.com>
2015-10-22 00:10:11 +0200

    stock data

--- stock_retrieval/views.py
+++ stock_retrieval/views.py
@@ -1,1 +1,21 @@
-__author__ = 'vincent'
+from django.views.generic import FormView, ListView
+
+from .forms import SyncFeedForm
+from .models import StockPrice, Stock
+from django.core.urlresolvers import reverse_lazy
+
+
+class StockPriceList(ListView):
+    template_name = 'stock_retrieval/index.html'
+    model = StockPrice
+
+
+class SyncFeed(FormView):
+    form_class = SyncFeedForm
+    success_url = reverse_lazy('stock-retrieval')
+
+    def form_valid(self, form):
+        Stock.get_stocks()
+        return super(SyncFeed, self).form_valid(form)
+
+
