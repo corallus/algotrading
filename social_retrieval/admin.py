d1bc8060f79988fc030f5aa3838c617f9be6f38d
Kees Hendriks <khendriks9@gmail.com>
2015-11-18 16:17:10 +0100

    added a few fields to the social_retrieval

--- social_retrieval/admin.py
+++ social_retrieval/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from social_retrieval.models import Tweet
+from social_retrieval.models import Tweet, Link
 
 admin.site.register(Tweet, admin.ModelAdmin)
+admin.site.register(Link, admin.ModelAdmin)
