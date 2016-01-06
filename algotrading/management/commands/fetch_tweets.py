e984af326b3bdcab8991b5f074b60514a4417052
Vincent van Bergen <v.vanbergen@gmail.com>
2016-01-06 18:55:05 +0100

    stukje code voor kees

--- algotrading/management/commands/fetch_tweets.py
+++ algotrading/management/commands/fetch_tweets.py
@@ -5,4 +5,5 @@ from stock_retrieval.models import Share
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for share in Share.objects.all():
+            share.get_share_display()
             pass
