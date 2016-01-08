af2620b5c045a41d0285fc92f31d08da9a4e810d
Vincent van Bergen <v.vanbergen@gmail.com>
2016-01-08 15:04:45 +0100

    meerdere feeds

--- news_retrieval/management/commands/fetch_news.py
+++ news_retrieval/management/commands/fetch_news.py
@@ -6,16 +6,22 @@ from stock_retrieval.models import Share
 from news_retrieval.models import NewsArticle
 from document.models import Document
 
+FEEDS = [
+    'http://feeds.finance.yahoo.com/rss/2.0/headline?s=%s&lang=en-US',
+    'https://news.google.com/news?q=%s&output=rss'
+]
+
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for share in Share.objects.all():
-            feed = feedparser.parse('http://feeds.finance.yahoo.com/rss/2.0/headline?s=%s&lang=en-US' % share.share)
-            for item in feed.entries:
-                if not NewsArticle.objects.filter(guid=item.guid).exists():
-                    document = Document(share=share)
-                    document.save()
-                    NewsArticle(document=document, guid=item.guid, title=item.title, link=item.link,
-                                description=item.description,
-                                published=datetime.strptime(item.published, '%a, %d %b %Y %H:%M:%S %Z')).save()
+            for feed in FEEDS:
+                feed = feedparser.parse(feed % share.share)
+                for item in feed.entries:
+                    if not NewsArticle.objects.filter(guid=item.guid).exists():
+                        document = Document(share=share)
+                        document.save()
+                        NewsArticle(document=document, guid=item.guid, title=item.title, link=item.link,
+                                    description=item.description,
+                                    published=datetime.strptime(item.published, '%a, %d %b %Y %H:%M:%S %Z')).save()
 
