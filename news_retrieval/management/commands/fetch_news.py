f7800818d70cbce768184c564961ca90e30f7443
Vincent van Bergen <v.vanbergen@gmail.com>
2016-01-15 14:26:58 +0100

    onnodige commands verwijderd

--- news_retrieval/management/commands/fetch_news.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from news_retrieval.utils import fetch
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        fetch()
-
