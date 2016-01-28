8ca1176c77858536f1d88f47d52ee7ec1d08828a
Vincent van Bergen <v.vanbergen@gmail.com>
2016-01-28 12:34:21 +0100

    fixed imports

--- sentiment/management/commands/classify.py
+++ sentiment/management/commands/classify.py
@@ -1,8 +1,5 @@
 from django.core.management.base import BaseCommand
 import time
-from openpyxl import Workbook
-import datetime
-from django.conf import settings
 
 from credibility.models import calculate_credibility
 from sentiment.utils import train, classify, predict
@@ -11,11 +8,10 @@ from sentiment.utils import train, classify, predict
 class Command(BaseCommand):
     def handle(self, *args, **options):
         while True:
-            accuracies = [datetime.datetime.now()]
             classifier, accuracy = train()
             # check whether there is training data to create a classifier
             if classifier:
-                classifier.show_most_informative_features()
+                # classifier.show_most_informative_features()
                 classify(classifier)  # classify unknown documents
                 calculate_credibility()
                 prediction = predict()
